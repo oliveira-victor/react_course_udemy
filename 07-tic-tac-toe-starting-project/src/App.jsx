afb7396078589dfa98b7a71c7f0222fc61a4ad12
oliveira-victor <victor.freire.oliveira@gmail.com>
2024-08-15 13:42:12 -0300

    disabling button after player's click

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -4,20 +4,27 @@ import Player from "./components/Player"
 import GameBoard from "./components/GameBoard"
 import Log from "./components/Log"
 
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = 'X'
+
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O'
+  }
+
+  return currentPlayer
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([])
-  const [activePlayer, setActivePlayer] = useState('X')
+  /* const [activePlayer, setActivePlayer] = useState('X') */
+
+  const activePlayer = deriveActivePlayer(gameTurns)
 
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X')
+    /* setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X') */
     setGameTurns(prevTurns => {
-      let currentPlayer = 'X'
-
-      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-        currentPlayer = 'O'
-      }
-
-      const updatedTurns = [{ square: {row: rowIndex, col: colIndex}, player: currentPlayer }, ...prevTurns]
+      const currentPlayer = deriveActivePlayer(prevTurns)
+      const updatedTurns = [{ square: { row: rowIndex, col: colIndex }, player: currentPlayer }, ...prevTurns]
 
       return updatedTurns
     })
@@ -30,8 +37,8 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
-        <GameBoard 
-          onSelectSquare={handleSelectSquare} 
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
           turns={gameTurns}
         />
       </div>
